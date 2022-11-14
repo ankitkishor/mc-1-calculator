b5353f4e2863819d7a0327291bf008f307c12941
ankit <ankit.kishorelv@gmail.com>
2022-11-14 15:08:05 +0530

    write a logic for ciel and floor maths logic

--- src/main/java/com/jap/calculator/ScientificCalculator.java
+++ src/main/java/com/jap/calculator/ScientificCalculator.java
@@ -6,20 +6,26 @@ public class ScientificCalculator {
 	{
 		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
 		//or else call ceil method of Math class and return the value
-
-		return 1;
+		if (!(num<=0)){
+			return Math.ceil(num);
+		}
+		else throw new CalculatorException("number can't be 0 or negative.");
 	}
 	
 	public double floorOfANumber(float num) throws CalculatorException
 	{
 		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
 		//or else call floor method of Math class and return the value
-		return 1;
+		if(!(num<=0)){
+			return Math.floor(num);
+		}
+		else throw new CalculatorException("number can't be 0 or negative.");
 	}
 	
     public long power(int num1, int num2) throws CalculatorException {
 		//if num1 is less than num2 than throw the userdefined exception with proper message
 		//else use Math pow method and return the value
+
 
 		return -1;
     }
