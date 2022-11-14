d1b14c71bcd352e50bf56ca4e8f6b37c59765170
ankit <ankit.kishorelv@gmail.com>
2022-11-14 15:06:09 +0530

    created try catch exception in method divide

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -3,16 +3,17 @@ package com.jap.calculator;
 public class MathematicalCalculator {
 
 	public int add(int num1, int num2)
-	{	
-		return num1+num2;
+	{
+		return num1 + num2;
 	}
 	public int subtract(int num1, int num2)
-	{	
-		return num1-num2;
+	{
+		return num1 - num2;
 	}
 	public int multiply(int num1, int num2)
-	{	
-		return num1*num2;
+	{
+
+		return num1 * num2;
 	}
 	public int divide(int num1, int num2)
 	{
@@ -26,10 +27,10 @@ public class MathematicalCalculator {
 		}catch (ArithmeticException exception){
 			System.out.println(exception);
 		}
-		return num1/num2;
+		return num1 / num2;
 	}
 	public int modulo(int num1, int num2)
-	{	
-		return num1%num2;
+	{
+		return num1 % num2;
 	}
 }
